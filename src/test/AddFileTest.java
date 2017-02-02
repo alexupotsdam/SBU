d42c5493dea0c3ebefb1b2827f87e6dc79486340
Leonard <leschrec@gmail.com>
2017-02-02 23:40:44 +0100

    add test

---
+++ src/test/AddFileTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.URL;
+
+import org.junit.Test;
+
+import model.Model;
+import ui.Constants;
+import view.View;
+
+public class AddFileTest {
+	
+
+	@Test
+	public void test() {
+		
+		String username = "b";
+		Model testModel = new Model();
+		View testView = new View(testModel,username);
+		
+		URL testFilePath = getClass().getClassLoader().getResource("raw/shared.png");
+		File testFile = new File(testFilePath.getFile());
+		
+		//Aufruf der Funktion zum Datei hochladen
+		testModel.addFile(username, testFile);
+			
+		String uploadedFilePath = Constants.uploadPathRoot + username + File.separator + "shared.png";
+		File uploadedFile = new File(uploadedFilePath);
+		
+		//überprüft ob shared.png in der Dateiliste ist und ob es im Temporären Verzeichnis liegt
+		assertTrue(testModel.files(username).contains("shared.png") && uploadedFile.exists());		
+	}
+
+}
